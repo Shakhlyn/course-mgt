e7a90f8329b4ca1492a551954d02255e9ec1884d
Shakhlyn <shakhlyn.sh.du@gmail.com>
2024-08-13 19:09:24 +0600

    feat: connect postgresql db

---
+++ db/models/user.py
@@ -0,0 +1,39 @@
+import enum
+
+from sqlalchemy import Boolean, Column, ForeignKey, Integer, String, Enum, Text
+from sqlalchemy.orm import relationship
+from sqlalchemy.sql.expression import null
+
+from ..db_setup import Base
+from .mixins import Timestamp
+
+class Role(enum.Enum):
+    teacher = 1
+    student = 2
+
+class User(Timestamp, Base):
+    __tablename__ = "users"
+
+    id = Column(Integer, primary_key= True, index=True)
+    email= Column(String(100), unique=True, index=True, nullable=False)
+    role = Column(Enum(Role))
+
+    profile = relationship("Profile", back_populates='owner', uselist=False)    # here, 'profile' is the class name; 'owner' is the relation type  
+    # here, the useList express the type of relationship: 1-to-1, 1-to-many
+    #By default it is 1-to-many. But if it is false, it denotes 1-to-1
+
+
+class Profile(Timestamp, Base):
+    __tablename__ = "profiles"
+
+    id = Column(Integer, primary_key= True, index=True)
+    first_name= Column(String(50), nullable=False)
+    last_name= Column(String(50), nullable=False)
+    bio = Column(Text, nullable=True)
+    is_active = Column(Boolean, default=False)
+    user_id = Column(Integer, ForeignKey("users.id"), nullable=False)
+
+    owner = relationship("User", back_populates='profile')
+
+
+
--- main.py
+++ main.py
@@ -1,14 +1,20 @@
 from fastapi import FastAPI
 
 from api import users, courses, sections
+from db.db_setup import engine
+
+from db.models import user, course
+
+user.Base.metadata.create_all(bind=engine)
+course.Base.metadata.create_all(bind=engine)
 
 app = FastAPI(
-    title="Fast API LMS",
-    description="LMS for managing students and courses.",
+    title="Course mgt",
+    description="web app for managing students and courses.",
     version="0.0.1",
     contact={
-        "name": "Gwen",
-        "email": "gwen@example.com",
+        "name": "Shakhlyn",
+        "email": "Shakhlyn@example.com",
     },
     license_info={
         "name": "MIT",
